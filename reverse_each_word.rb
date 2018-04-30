edbc5753fccf9068f6a05fa56eaf3dce5dcbd451
Paul <pcherian10@gmail.com>
2018-04-30 20:28:48 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,8 @@
 def reverse_each_word (string)
   string = string.split()
-  puts string
   result = []
   string.each {|word| result << word.reverse}
-  result.to_s
+  puts result.to_s
 end
 
 reverse_each_word("Hello there, and how are you?")
